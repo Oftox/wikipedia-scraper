9926d715750049093c7facfdc6a644249fb08e5f
Oftox <oftox@protonmail.com>
2022-06-12 21:59:46 +0300

    added article body to output

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-#--------------------------------
-#Created by Oftox 
-#Date: 06.2022 (MM/YYYY)
-#--------------------------------
 import sys
 import requests
 from bs4 import BeautifulSoup
@@ -48,6 +44,7 @@ requested = requests.get("https://" + language + ".wikipedia.org")
 if (requested.status_code >= 400):
 	print("Error: could not connect to " + language +".wikipedia.org")
 	sys.exit()
+requested = None
 
 
 #create translation variables from the "translations.txt" file
@@ -73,19 +70,57 @@ free = lines[abbrLine + 2].replace("\n", '')
 #start main loop with "running" as the flag
 running = True
 while (running):
-	#search page with input 
-	print("\n")
-	print("\t\t" + welcome)
-	print("    " + free)
-	print("\n")
-
 	try:
-		input("    " + ": ")
+		print("\n")
+		print("\t\t" + welcome)
+		print("    " + free)
+		print("\n")
+		
+		
+		#get user input and check validity
+		searchValid = False
+		while (searchValid == False):	
+			search = input("    " + ": ")
+			if (search == "" or search == " "):
+				print("")
+				#TODO
+				print("\tSearch cannot be empty. Try again.")
+				print("")
+			else:
+				searchValid = True
+				print("\n")
+
+		print("-" * 60)
+		
+		
+		#connect to wikipedia and print article if found
+		requested = requests.get("https://" + language + ".wikipedia.org/w/index.php?search=" + search)
+		if (requested.status_code >= 400):
+			print("Error: could not connect to " + language +".wikipedia.org")
+			sys.exit()
+		page = BeautifulSoup(requested.text, 'html.parser')
+
+		resultsinfo = page.find(class_='oo-ui-fieldLayout-field')
+		if(resultsinfo != None):
+			#TODO
+			print("\n")
+			print("\t\tArticle not found")
+			print("\n")		
+		else:
+			firstheading = page.find(class_='firstHeading')
+			print("\n")
+			print("\t\t" + content2str(firstheading.contents))
+			print("\n")
+			
+			paragraphs = page.find_all('p')
+			for i in range(len(paragraphs)):
+				print(content2str(paragraphs[i].contents))
+				
+		print("-" * 60) 
+		
 	except KeyboardInterrupt:
 		print("\n")
 		running = False
-	
-	running = False;
 
 
 
